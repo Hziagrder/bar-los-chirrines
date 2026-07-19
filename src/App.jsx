dfa2bc8a5f92855073dda75685ebb9ae2f1fb422
Hziagrder <hugocaparros2008@gmail.com>
2026-07-19 16:34:19 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Hero from './components/Hero.jsx'
 import About from './components/About.jsx'
 import Delivery from './components/Delivery.jsx'
 import Menu from './components/Menu.jsx'
+import Reviews from './components/Reviews.jsx'
+import Football from './components/Football.jsx'
 import Reservations from './components/Reservations.jsx'
 import Location from './components/Location.jsx'
 import Footer from './components/Footer.jsx'
@@ -17,6 +19,8 @@ export default function App() {
         <About />
         <Delivery />
         <Menu />
+        <Reviews />
+        <Football />
         <Reservations />
         <Location />
       </main>
